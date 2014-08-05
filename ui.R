6201ae602969b69289e4bcd9b2e04f44e9b0c0c6
David Monk <lostintunisia@gmail.com>
2014-08-04 17:54:36 -0700

    minor UI updates

--- ui.R
+++ ui.R
@@ -3,11 +3,15 @@ outcomes <<- c("heart attack", "heart failure", "pneumonia")
 
 shinyUI(pageWithSidebar(
   headerPanel("USA hospital recommendations"),
-  sidebarPanel(
-    helpText("Select your U.S. state and Outcome (medical treatment)"),
-    selectInput("selectState", "State", choices = usState ),
-    selectInput("selectOutcome", "Outcome (Treatment Type)", choices = outcomes ),
-    helpText("Data: Hospital Compare, http://hospitalcompare.hhs.gov/")
+  sidebarPanel(tabsetPanel(
+    tabPanel("Application",
+             selectInput("selectState", "State", choices = usState ),
+             selectInput("selectOutcome", "Outcome (Treatment Type)", choices = outcomes )
+    ),
+    tabPanel("Instructions",
+             helpText("Select your U.S. state and Outcome (medical treatment) to see the recommended hospital"),
+             helpText("Data: Hospital Compare, http://hospitalcompare.hhs.gov/" )
+  )
   ),
   mainPanel(
     h3('Hospital with lowest 30 day post treatment mortality'),
